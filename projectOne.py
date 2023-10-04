827ece805c7dc01fad53fec20acd59b5fb2f9124
yashpat1602 <ysp21@scarletmail.rutgers.edu>
2023-10-04 15:38:28 -0400

    yash

--- projectOne.py
+++ projectOne.py
@@ -20,14 +20,16 @@ def adjacent(x, y): #finds the neighbors of a cell by finding coordinates left/r
         x_right = -1
     
     if y > 0:
-        y_left = y - 1
+        y_up = y - 1
     else: 
-        y_left = -1
+        y_up = -1
     
     if y < len(grid[x]) - 1:
-        y_right = y + 1
+        y_down = y + 1
     else:
-        y_right = -1
+        y_down = -1
+
+    return x_left, x_right, y_up, y_down
 
 random_x = random.randint(0, d - 1)
 random_y = random.randint(0, d - 1)
@@ -36,4 +38,20 @@ grid[random_x][random_y] = 1 # open random square
 possible = True
 
 while possible:
+    blocked = []
+    for x in range(d):
+        for y in range(d):
+            left, right, up, down = adjacent(x,y)
+            if (left != -1 and right == -1 and up == -1 and down == -1) or (left == -1 and right != -1 and up == -1 and down == -1) or (left == -1 and right == -1 and up != -1 and down == -1) or (left == -1 and right == -1 and up == -1 and down != -1):
+                blocked.append([x,y])
+    
+    if len(blocked) == 0:
+        break
+
+    random_pick = random.randint(0, len(blocked) - 1) # randomly choose one of the blocked cells with exactly one neighbor
+    x1, y1 = random_pick
+    grid[x1][y1] = 1 # open the chosen cell
+
+    
+
     
