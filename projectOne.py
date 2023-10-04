f5480f659e4d3b5f8208a4ed45be9cc03a44349c
yashpat1602 <ysp21@scarletmail.rutgers.edu>
2023-10-04 15:07:23 -0400

    yash

--- projectOne.py
+++ projectOne.py
@@ -9,22 +9,22 @@ for x in range(d):
 
 
 def adjacent(x, y): #finds the neighbors of a cell by finding coordinates left/right/up/down of given cell
-    if (x > 0):
+    if x > 0:
         x_left = x - 1
     else:
         x_left = -1
 
-    if (x < len(grid) - 1):
+    if x < len(grid) - 1:
         x_right = x + 1
     else:
         x_right = -1
     
-    if (y > 0):
+    if y > 0:
         y_left = y - 1
     else: 
         y_left = -1
     
-    if (y < len(grid[x]) - 1):
+    if y < len(grid[x]) - 1:
         y_right = y + 1
     else:
         y_right = -1
@@ -32,3 +32,8 @@ def adjacent(x, y): #finds the neighbors of a cell by finding coordinates left/r
 random_x = random.randint(0, d - 1)
 random_y = random.randint(0, d - 1)
 grid[random_x][random_y] = 1 # open random square
+
+possible = True
+
+while possible:
+    
