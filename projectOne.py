f677229ad6561ba2c2ac745af86e513ce801ced2
yashpat1602 <ysp21@scarletmail.rutgers.edu>
2023-10-04 14:59:00 -0400

    yash

--- projectOne.py
+++ projectOne.py
@@ -5,3 +5,24 @@ for x in range(d):
     for y in range(d):
         grid[x][y] = 0 # 0 signifies empty, while 1 signifies open cell
 
+
+def adjacent(x, y):
+    if (x > 0):
+        x_left = x - 1
+    else:
+        x_left = -1
+
+    if (x < len(grid) - 1):
+        x_right = x + 1
+    else:
+        x_right = -1
+    
+    if (y > 0):
+        y_left = y - 1
+    else: 
+        y_left = -1
+    
+    if (y < len(grid[x]) - 1):
+        y_right = y + 1
+    else:
+        y_right = -1
