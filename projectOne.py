cba7081a75cf0b8e28f780b9c748bff56acb1b54
yashpat1602 <ysp21@scarletmail.rutgers.edu>
2023-10-07 21:45:08 -0400

    y

--- projectOne.py
+++ projectOne.py
@@ -4,11 +4,11 @@ import collections
 #grid = [[1 for i in range(d)] for i in range(d)]
 d = 10 # size of grid
 q = 0.5
+k = 0
 
 # creates the grid
 grid = [[0 for i in range(d)] for i in range(d)] # 0 signifies blocked, while 1 signifies open cell
 openCells = []
-
 
 # finds the open neighbors of a cell by finding coordinates left/right/up/down of given cell
 def adjacent(x, y):
@@ -52,21 +52,12 @@ def init_grid():
     start_y = random.randint(0, d - 1)
     grid[start_x][start_y] = 1
     openCells.append((start_x, start_y))
-# open a random square in the grid
-start_x = random.randint(0, d - 1)
-start_y = random.randint(0, d - 1)
-grid[start_x][start_y] = 1
 
     # identify all blocked cells with exactly 1 open neighbor and randomly choose one of them to open
     oneOpenNeighbor = []
     for (r,c) in [(1,0), (-1,0), (0,-1), (0, 1)]:
         if validCell(start_x + r, start_y + c):
             oneOpenNeighbor.append((start_x + r, start_y + c))
-# identify all blocked cells with exactly 1 open neighbor and randomly choose one of them to open
-oneOpenNeighbor = []
-for (r,c) in [(1,0), (-1,0), (0,-1), (0, 1)]:
-    if start_x + r >= 0 and start_x + r < d and start_y + c >= 0 and start_y + c < d:
-        oneOpenNeighbor.append((start_x + r, start_y + c))
 
     while len(oneOpenNeighbor) != 0:
             randIndex = random.randint(0, len(oneOpenNeighbor) - 1)
@@ -80,28 +71,17 @@ for (r,c) in [(1,0), (-1,0), (0,-1), (0, 1)]:
                         oneOpenNeighbor.remove((selected_x + r, selected_y + c))
                     else:
                         oneOpenNeighbor.append((selected_x + r, selected_y + c))
-while len(oneOpenNeighbor) != 0:
-        randIndex = random.randint(0, len(oneOpenNeighbor) - 1)
-        selected_x, selected_y = oneOpenNeighbor[randIndex]
-        grid[selected_x][selected_y] = 1
-        oneOpenNeighbor.remove((selected_x, selected_y))
-        for (r,c) in [(1,0), (-1,0), (0,-1), (0, 1)]:
-            if selected_x + r >= 0 and selected_x + r < d and selected_y + c >= 0 and selected_y + c < d and grid[selected_x + r][selected_y + c] == 0:
-                if (selected_x + r, selected_y + c) in oneOpenNeighbor:
-                    oneOpenNeighbor.remove((selected_x + r, selected_y + c))
-                else:
-                    oneOpenNeighbor.append((selected_x + r, selected_y + c))
 
-# identify all "dead end" cells
-deadend = []
-for x in range(d):
-    for y in range(d):
-        left, right, up, down = adjacent(x,y)
-        if grid[x][y] == 1 and ((left != -1 and right == -1 and up == -1 and down == -1) or (left == -1 and right != -1 and up == -1 and down == -1) or (left == -1 and right == -1 and up != -1 and down == -1) or (left == -1 and right == -1 and up == -1 and down != -1)):
-            deadend.append((x,y))
+    # identify all "dead end" cells
+    deadend = []
+    for x in range(d):
+        for y in range(d):
+            left, right, up, down = adjacent(x,y)
+            if grid[x][y] == 1 and ((left != -1 and right == -1 and up == -1 and down == -1) or (left == -1 and right != -1 and up == -1 and down == -1) or (left == -1 and right == -1 and up != -1 and down == -1) or (left == -1 and right == -1 and up == -1 and down != -1)):
+                deadend.append((x,y))
 
-# randomly open one closed neighbor of approximately half of the "dead end" cells
-half = len(deadend) // 1.75
+    # randomly open one closed neighbor of approximately half of the "dead end" cells
+    half = len(deadend) // 1.75
 
     while len(deadend) > half:
         randIndex = random.randint(0, len(deadend) - 1)
@@ -115,17 +95,6 @@ half = len(deadend) // 1.75
         grid[neighborR][neighborC] = 1
         openCells.append((neighborR, neighborC))
         deadend.remove((selected_x, selected_y))
-while len(deadend) > half:
-    randIndex = random.randint(0, len(deadend) - 1)
-    selected_x, selected_y = deadend[randIndex]
-    neighbors = []
-    for (r,c) in [(1,0), (-1,0), (0,-1), (0, 1)]:
-                if selected_x + r >= 0 and selected_x + r < d and selected_y + c >= 0 and selected_y + c < d and grid[selected_x + r][selected_y + c] == 0:
-                        neighbors.append((selected_x + r, selected_y + c))
-    randNeighborIndex = random.randint(0, len(neighbors) - 1)
-    neighborR, neighborC = neighbors[randNeighborIndex]
-    grid[neighborR][neighborC] = 1
-    deadend.remove((selected_x, selected_y))
 
 fireCells = []
 adjToFireCells = []
@@ -264,18 +233,3 @@ def run_bots():
     print("Reset")
 
 run_bots()
-
-
-
-
-
-    
-
-                    
-
-
-
-
-
-
-
